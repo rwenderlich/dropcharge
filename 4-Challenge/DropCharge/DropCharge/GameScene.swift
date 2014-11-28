9eae44ddecbc25ec9cb85d892819842e38f14e13
Ray Wenderlich <ray.wenderlich@gmail.com>
2014-11-28 12:25:41 -0500

    Refactored to LevelNode

--- 4-Challenge/DropCharge/DropCharge/GameScene.swift
+++ 4-Challenge/DropCharge/DropCharge/GameScene.swift
@@ -46,8 +46,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   var dt: NSTimeInterval = 0
   var player = SKSpriteNode(imageNamed: "player01_fall_1.png")
   var gameState = GameState.WaitingForTap
-  var levelMaxY: CGFloat = 0
-  let shapeNode = SKShapeNode()
   var lava: SKNode = SKNode()
   var visibleMinYFg = CGFloat(0)
   var visibleMaxYFg = CGFloat(0)
@@ -57,7 +55,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   let title = SKSpriteNode(imageNamed: "DropCharge_title")
   let bomb = SKSpriteNode(imageNamed: "bomb_1")
   var playerTrail: SKEmitterNode! = nil
-  
   var animJump: SKAction! = nil
   var animFall: SKAction! = nil
   var animSteerLeft: SKAction! = nil
@@ -66,6 +63,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   var timeSinceLastExplosion: CGFloat = 0
   var timeForNextExplosion: CGFloat = 0
   var soundManager: SoundManager!
+  var level: LevelNode!
   
   // MARK: Init
   override func didMoveToView(view: SKView) {
@@ -95,7 +93,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     mgNode = RepeatableNode(prefix: "midground_", number: 4, containerSize: size)
     worldNode.addChild(mgNode)
     worldNode.addChild(fgNode)
-    fgNode.addChild(shapeNode)
+    level = LevelNode(size: size)
+    fgNode.addChild(level)
   }
   
   func setupPlayer() {
@@ -127,10 +126,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     title.position = CGPoint(x: size.width/2, y: size.height * 0.7)
     worldNode.addChild(title)
     
-    let chunk = create5Across(createPlatformNormalAtPosition)
-    chunk.position = CGPoint(x: 0, y: 50)
-    fgNode.addChild(chunk)
-    levelMaxY = size.height
   }
   
   func setupLava() {
@@ -233,165 +228,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   
   // MARK: Game Objects
   
-  func createCoinNormalAtPosition(position: CGPoint) -> SKSpriteNode {
-    let sprite = SKSpriteNode(imageNamed: "powerup05_1")
-    sprite.position = position
-    sprite.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
-    sprite.physicsBody?.dynamic = false
-    sprite.physicsBody?.categoryBitMask = PhysicsCategory.CoinNormal
-    sprite.physicsBody?.collisionBitMask = 0
-    sprite.physicsBody?.contactTestBitMask = PhysicsCategory.Player
-    return sprite
-  }
-  
-  func createCoinSpecialAtPosition(position: CGPoint) -> SKSpriteNode {
-    let sprite = SKSpriteNode(imageNamed: "powerup01_1")
-    sprite.position = position
-    sprite.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
-    sprite.physicsBody?.dynamic = false
-    sprite.physicsBody?.categoryBitMask = PhysicsCategory.CoinSpecial
-    sprite.physicsBody?.collisionBitMask = 0
-    sprite.physicsBody?.contactTestBitMask = PhysicsCategory.Player
-    return sprite
-  }
-  
-  func createPlatformNormalAtPosition(position: CGPoint) -> SKSpriteNode {
-    let sprite = SKSpriteNode(imageNamed: "platform01_1")
-    sprite.position = position
-    sprite.physicsBody = SKPhysicsBody(rectangleOfSize: sprite.size)
-    sprite.physicsBody?.dynamic = false
-    sprite.physicsBody?.categoryBitMask = PhysicsCategory.PlatformNormal
-    sprite.physicsBody?.collisionBitMask = 0
-    sprite.physicsBody?.contactTestBitMask = PhysicsCategory.Player
-    return sprite
-  }
-  
-  func createPlatformBreakableAtPosition(position: CGPoint) -> SKSpriteNode {
-    let sprite = SKSpriteNode(imageNamed: "block_break01")
-    sprite.position = position
-    sprite.physicsBody = SKPhysicsBody(rectangleOfSize: sprite.size)
-    sprite.physicsBody?.dynamic = false
-    sprite.physicsBody?.categoryBitMask = PhysicsCategory.PlatformBreakable
-    sprite.physicsBody?.collisionBitMask = 0
-    sprite.physicsBody?.contactTestBitMask = PhysicsCategory.Player
-    return sprite
-  }
   
   // MARK: Level Spawning
   
-  func levelSpawnRandom() {
     
-    let randNum = Int.random(min: 0, max: 4)
-    
-    var chunks = [SKSpriteNode]()
-    if randNum == 0 {
-      chunks.append(create5Across(createPlatformNormalAtPosition))
-    } else if randNum == 1 {
-      chunks.append(create5Across(createPlatformBreakableAtPosition))
-    }
-    else if randNum == 2 {
-      let platformSpawner = Int.random(min: 0, max: 1) == 0 ? createPlatformNormalAtPosition : createPlatformBreakableAtPosition
-      let chunk1 = createSingle(platformSpawner)
-      let chunk2 = createSingle(createCoinNormalAtPosition)
-      for node in chunk2.children {
-        if node.position.y > chunk1.size.height {
-          node.removeFromParent()
-        }
-      }
-      chunks.append(chunk1)
-      chunks.append(chunk2)
-    } else if randNum == 3 {
-       chunks.append(createCross(createCoinNormalAtPosition, createCoinSpecialAtPosition))
-    } else {
-       chunks.append(createLine(createCoinNormalAtPosition, createCoinSpecialAtPosition))
-    }
-    
-    for chunk in chunks {
-      chunk.position = CGPointMake(0, levelMaxY)
-      fgNode.addChild(chunk)
-    }
-    levelMaxY += chunks[0].size.height
-
-  }
-  
-  func levelSpawnTest() -> SKSpriteNode {
-    let chunk = SKSpriteNode()
-    chunk.addChild(createCoinNormalAtPosition(CGPoint(x: 160, y: 0)))
-    chunk.addChild(createCoinNormalAtPosition(CGPoint(x: 160, y: 50)))
-    chunk.addChild(createCoinNormalAtPosition(CGPoint(x: 160, y: 100)))
-    chunk.addChild(createCoinSpecialAtPosition(CGPoint(x: 160, y: 150)))
-    chunk.addChild(createPlatformNormalAtPosition(CGPoint(x: 160, y: 200)))
-    chunk.addChild(createPlatformBreakableAtPosition(CGPoint(x: 160, y: 250)))
-    chunk.size = CGSize(width: size.width, height: 350)
-    return chunk
-  }
-  
-  func create5Across(spawner:(CGPoint) -> SKSpriteNode) -> SKSpriteNode {
-    let chunk = SKSpriteNode()
-    for i in 0...5 {
-      chunk.addChild(spawner(CGPoint(x: 40 + 60 * i, y: 0)))
-    }
-    chunk.size = CGSize(width: size.width, height: 100)
-    return chunk
-  }
-  
-  func createSingle(spawner:(CGPoint) -> SKSpriteNode) -> SKSpriteNode {
-    let chunk = SKSpriteNode()
-    
-    let randomNumSingles = Int.random(min: 5, max: 20)
-    for i in 0...randomNumSingles {
-      chunk.addChild(spawner(CGPoint(x: CGFloat.random(min: 0, max: size.width), y: 50.0*CGFloat(i))))
-    }
-
-    chunk.size = CGSize(width: size.width, height: 50 * CGFloat(randomNumSingles) + 100)
-    
-    return chunk
-  }
-  
-  func createCross(spawner1:(CGPoint) -> SKSpriteNode, spawner2:(CGPoint) -> SKSpriteNode) -> SKSpriteNode {
-    let chunk = SKSpriteNode()
-    
-    let randomX = CGFloat.random(min: 50, max: size.width - 100)
-    
-    chunk.addChild(spawner1(CGPoint(x: randomX, y: 100)))
-    chunk.addChild(spawner1(CGPoint(x: randomX - 50, y: 50)))
-    chunk.addChild(spawner2(CGPoint(x: randomX, y: 50)))
-    chunk.addChild(spawner1(CGPoint(x: randomX + 50, y: 50)))
-    chunk.addChild(spawner1(CGPoint(x: randomX, y: 0)))
-    
-    chunk.size = CGSize(width: size.width, height: 150)
-    
-    return chunk
-  }
-  
-  func createLine(spawner1:(CGPoint) -> SKSpriteNode, spawner2:(CGPoint) -> SKSpriteNode) -> SKSpriteNode {
-    let chunk = SKSpriteNode()
-    
-    let numToSpawn = Int.random(min: 5, max: 30)
-    var startX = CGFloat.random(min: 50, max: size.width-50)
-    var direction = CGFloat(1.0)
-    
-    for i in 0..<numToSpawn {
-
-      let yPosition = 50.0*CGFloat(i)
-      let spawner = i == numToSpawn-1 ? spawner2 : spawner1
-    
-      chunk.addChild(spawner(CGPoint(x: startX, y: yPosition)))
-      chunk.addChild(spawner(CGPoint(x: startX + 40, y: yPosition)))
-      
-      if (direction > 0 && startX + 100 > size.width) ||
-         (direction < 0 && startX < 50) {
-        direction *= -1
-      }
-      startX = startX + (50 * direction)
-
-    }
-    
-    chunk.size = CGSize(width: size.width, height: 50.0*CGFloat(numToSpawn) + 100)
-    
-    return chunk
-  }
-  
   // MARK: Touch Handling
   
   func handlePlayingTouches(touches: NSSet) {
@@ -441,7 +281,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
       updateVisible()
       bgNode.update()
       mgNode.update()
-      updateLevel()
+      level.update()
 
       updatePlayer()
       updateLava()
@@ -480,30 +320,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     visibleMaxYFg = convertPoint(upperLeft, toNode: fgNode).y
 
   }
-  
-  func updateLevel() {
     
-//    let path = CGPathCreateMutable()
-//    CGPathMoveToPoint(path, nil, 0, visibleMinYFg)
-//    CGPathAddLineToPoint(path, nil, size.width, visibleMinYFg)
-//    shapeNode.path = path
-//    shapeNode.strokeColor = SKColor.redColor()
-//    shapeNode.lineWidth = 4.0
-    
-    while visibleMaxYFg > levelMaxY {
-      levelSpawnRandom()
-    }
-    
-    for node in fgNode.children {
-      if let sprite = node as? SKSpriteNode {
-        if sprite.position.y + sprite.size.height < visibleMinYFg - size.height {
-          sprite.removeFromParent()
-        }
-      }
-    }
-    
-  }
-  
   func updatePlayer() {
   
     // Set velocity based on core motion
